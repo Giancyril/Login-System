8cd9a0700e93b7869fecb97ee662eee2fc53a702
Giancyril <mijaresgiancyri@gmail.com>
2025-04-22 08:30:49 +0800

    changed the title

--- loggedin.php
+++ loggedin.php
@@ -17,7 +17,7 @@ if (!isAuthenticated()) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Logged In!</title>
+    <title>Homepage</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="tailwind.config.js"></script>
     <link rel="stylesheet" href="css/custom.css">
